97efd8afea3a37a4e6a983b3c80195fcc2b63cdb
Mupagasi Jean Paul <jeanpaul.mupagasi@cureinternational.org>
2019-04-21 09:50:48 +0200

    customize email body trello: https://trello.com/c/vAJnFUnX

--- kijabe_purchase_ext/models/purchase.py
+++ kijabe_purchase_ext/models/purchase.py
@@ -28,30 +28,31 @@ class purchase(models.Model):
     @api.multi
     def button_confirm(self):
         super(purchase, self).button_confirm()  # confirm order
-        # get groups of 'Manager' in res.groups
-        groups = self.env['res.groups'].search([['name', '=', "Manager"]])
-        for group in groups:
-            users = self.env["res.users"].search([['groups_id', '=', group.id], [
-                                                 'active', '=', True]])  # get active users who belong in the group ids
-        # get emails from the user object
-        recipients = ','.join(str(user.login) for user in users)
-
+        # get current url
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
         if self.state == 'to approve':
-            # call the method to send emails
-            self._send_mail(recipients, self[0].name)
+            # get groups of 'Manager' in res.groups
+            groups = self.env['res.groups'].search([['name', '=', "Manager"]])
+            for group in groups:
+                users = self.env["res.users"].search([['groups_id', '=', group.id], [
+                    'active', '=', True]])  # get active users who belong in the group ids
+
+            for user in users:
+                # call the method to send emails
+                self._send_mail(user.login, self[0].name, user.name,base_url)
         return True
 
     # Send email
-    def _send_mail(self, recipients, po):
+    def _send_mail(self, recipient, po, name,url):
         mail_pool = self.env['mail.mail']
         values = {}
         values.update({'subject': 'Purchase Order #' +
                        po + ' waiting your approval'})
         values.update({'email_from': "odoomail.service@gmail.com"})
-        values.update({'email_to': recipients})
+        values.update({'email_to': recipient})
         values.update({'body_html':
-                       'To Manager,<br>'
-                       + 'LPO No. ' + po + ' has been created and requires your approval. You can find the details to approve here.'})
+                       'To Manager ' + name + ',<br>'
+                       + 'LPO No. ' + po + ' has been created and requires your approval. You can find the details to approve here. '+url})
 
         msg_id = mail_pool.create(values)
         if msg_id:
